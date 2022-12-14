2ba8695ebb6e2b993447120aecc65fd70da7ac64
al-mahfuz <almahfuzhossen@gmail.com>
2022-12-14 20:42:45 +0600

    API

--- Js/api.js
+++ Js/api.js
@@ -9,3 +9,32 @@ function loadData(){
     .then(response => response.json())
     .then(data => console.log(data))
 }
+
+/* function loadUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => console.log(data));
+} */
+
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => displayUsers(data));
+}
+
+function loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => console.log(data));
+}
+
+function displayUsers(data) {
+    const ul = document.getElementById('users');
+    // console.log(data);
+    for (const user of data) {
+        console.log(user);
+        const li = document.createElement('li');
+        li.innerText = `name: ${user.name} email: ${user.email}`;
+        ul.appendChild(li);
+    }
+}
